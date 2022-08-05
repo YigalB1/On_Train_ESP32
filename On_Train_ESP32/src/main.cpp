01f675e1b4b91b1f218c15fb4e299db949a99f9e
YigalB1 <yigalb@hotmail.com>
2022-08-05 10:00:03 +0300

    More

--- On_Train_ESP32/src/main.cpp
+++ On_Train_ESP32/src/main.cpp
@@ -1,18 +1,115 @@
 #include <Arduino.h>
 
-int OutLed_Pin = 0 ; // D3 == GPIO0
-int IN1A_Pin = 12 ;  // D6 == GPIO12
-int IN1B_Pin = 13 ;  // D7 == GPIO13
-int IN2A_Pin =  4 ;  // D2 == GPIO4
-int IN2B_Pin =  5 ;  // D1 == GPIO5
-int EN1_Pin  = 14 ;  // D5 == GPIO14
-int EN2_Pin  =  2 ;  // D4 == GPIO2
+// LED1 is power ...
+int Led2_yellow = 21 ; 
+int Led3_blue =   18 ; 
+int Led4_green =   5 ; 
+int Buzzer     =  23 ; 
+
+int EN1_Pin  = 35 ;  
+int IN1A_Pin = 32 ;
+int IN1B_Pin = 14 ;  
+
+int EN2_Pin  =  2 ;  
+int IN2A_Pin =  19 ;  
+int IN2B_Pin =  4 ;  
+
+
 bool state = true;
 
+bool tmp = HIGH;
+
+
+
+
+class motor_control {
+
+  public:
+  int en;
+  int in_1;
+  int in_2;
+  // each instance of class will have different motor
+  // in this case we have one motor
+
+void stop() {
+    digitalWrite(in_1, LOW); // 
+    digitalWrite(in_2, LOW); //
+    digitalWrite(EN1_Pin,LOW); 
+    digitalWrite(Led2_yellow, HIGH);
+    digitalWrite(Led3_blue, LOW);  
+    digitalWrite(Led4_green, LOW);
+    digitalWrite(Buzzer, LOW);
+  } // go_fwd()
+
+  void go_fwd(int _speed) {
+    digitalWrite(Led2_yellow, LOW);
+    digitalWrite(Led3_blue, HIGH);  
+    digitalWrite(Led4_green, LOW);
+    digitalWrite(in_1, LOW); // 
+    digitalWrite(in_2, HIGH); //
+    digitalWrite(en,HIGH); 
+  } // go_fwd()
+
+    void go_back(int _speed) {
+      digitalWrite(Led2_yellow, LOW);
+    digitalWrite(Led3_blue, LOW);  
+    digitalWrite(Led4_green, HIGH);
+    digitalWrite(in_1, HIGH); // 
+    digitalWrite(in_2, LOW); // 
+    digitalWrite(en,HIGH);
+  } // go_back()
+
+}; // of class motor_control
+
+
+
+motor_control motor_1;
+motor_control motor_2;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(Led2_yellow, OUTPUT);
+  pinMode(Led3_blue, OUTPUT);
+  pinMode(Led4_green, OUTPUT);
+  pinMode(Buzzer, OUTPUT);
+
+  motor_1.en = EN1_Pin;
+  motor_1.in_1 = IN1A_Pin;
+  motor_1.in_2 = IN2A_Pin;
+  motor_1.stop();
+
+  //digitalWrite(Led2_yellow, LOW);
+  //digitalWrite(Led3_blue, LOW);  
+  //digitalWrite(Led4_green, LOW);
+  //digitalWrite(Buzzer, LOW);
+
+  Serial.begin(9600);
+  tmp = HIGH;
+
+// disable notors
+//motor_1.en = LOW;
+//motor_1.in_1 = LOW;
+//motor_1.in_2 = LOW;
+//motor_2.en = LOW;
+//motor_2.in_1 = LOW;
+//motor_2.in_2 = LOW;
+Serial.println("end of Setup()");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int tmp_speed=777;
+  
+
+  motor_1.go_fwd(tmp_speed);
+  delay(2000);
+
+  motor_1.stop();
+  delay(1000);
+
+  motor_1.go_back(tmp_speed);
+  delay(2000);
+
+  motor_1.stop();
+  delay(1000);
+  
 }
+
