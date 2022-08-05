1bd781c3a331e6f177fbcb640189452ad88ad806
YigalB1 <yigalb@hotmail.com>
2022-08-05 09:47:12 +0300

    work 1

---
+++ On_Train_ESP32/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+int OutLed_Pin = 0 ; // D3 == GPIO0
+int IN1A_Pin = 12 ;  // D6 == GPIO12
+int IN1B_Pin = 13 ;  // D7 == GPIO13
+int IN2A_Pin =  4 ;  // D2 == GPIO4
+int IN2B_Pin =  5 ;  // D1 == GPIO5
+int EN1_Pin  = 14 ;  // D5 == GPIO14
+int EN2_Pin  =  2 ;  // D4 == GPIO2
+bool state = true;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
